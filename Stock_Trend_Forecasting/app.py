bbfde6b19c5b9e531016dbe1f64021c5baffcbae
balusena <57207778+balusena@users.noreply.github.com>
2024-06-14 21:34:23 +0100

    Update app.py changes done

--- Stock_Trend_Forecasting/app.py
+++ Stock_Trend_Forecasting/app.py
@@ -27,7 +27,7 @@ period = n_years * 365
 
 @st.cache_data
 def load_data(ticker):
-    data = yf.download(ticker, START, TODAY)
+    data = yf.download(ticker, START, TODAY, progress=False)
     data.reset_index(inplace=True)
     data['Date'] = pd.to_datetime(data['Date'])
     return data
